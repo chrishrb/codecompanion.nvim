8d30b893a0712842e70c32b480ad60b1a8d3b442
Christoph Herb <52382992+chrishrb@users.noreply.github.com>
2024-09-18 09:44:24 +0200

    force absolute paths

--- lua/codecompanion/helpers/tools/editor.lua
+++ lua/codecompanion/helpers/tools/editor.lua
@@ -138,7 +138,7 @@ Or, Consider the following example which replaces content between the lines 10 a
 ```
 
 Or, Consider the following example which creates or saves a new file with the path /Users/christoph/hello.py and content print('Hello CodeCompanion'). The file path should
-be the same absolute path as the current buffer unless the path is specified by the user:
+be the same path as the current buffer unless the path is specified by the user. Use always absolute paths like /Users/christoph/hello.py and not relative paths like ./hello.py:
 
 ```xml
 %s
